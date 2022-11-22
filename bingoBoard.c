45dcb336cbf35e4ae2bf309284222ac7e83bfdeb
myoostis <parkgin2011@naver.com>
2022-11-22 17:44:33 +0900

    01

--- bingoBoard.c
+++ bingoBoard.c
@@ -4,13 +4,20 @@
 #define BINGONUM_HOLE       -1
 
 static int bingoBoard[N_SIZE][N_SIZE];
-static int numberStatus[N_SIZE][N_SIZE];
+static int numberStatus[N_SIZE*N_SIZE];
 
+int bingo_checkNum(int selNum)
+{
+	if(numberStatus[selNum-1] == BINGONUM_HOLE)
+	     return BINGO_NUMSTATUS_ABSENT;
+     return BINGO_NUMSTATUS_PRESENT;
+}
 
 void bingo_init(void)
 {
 	int i, j;
-	int cnt =1;
+	int cnt = 1;
+	
 	
 	for(i=0; i<N_SIZE; i++){
 		for(j=0; j<N_SIZE; j++)
@@ -19,13 +26,12 @@ void bingo_init(void)
 			{
 				bingoBoard[i][j] = BINGONUM_HOLE;
 				numberStatus[cnt-1] = BINGONUM_HOLE;
-				
 				cnt++;
 			}
 			else
 			{
 				numberStatus[cnt-1] = i*N_SIZE + j; // 증가시키전에 값을 넣어버림. 
-				bingoBoard[i][j] = cnt++;	// cnt값을 빙고보드에 넣고 cnt값은 증가시키고 
+				bingoBoard[i][j] = cnt++;	// cnt값을 빙고보드에 넣고 cnt값은 증가
 			} 
 		}
 	}
@@ -50,14 +56,70 @@ void bingo_print(void)
 
 void bingo_inputNum(int sel)
 {
-	 int row_index, col_index;
-	 
-	 row_index = numberStatus[sel-1]/N_SIZE;
-	 col_index = numberStatus[sel-1]/N_SIZE;
-	 
+	int row, col;
+	row = numberStatus[sel-1]/N_SIZE;
+	col = numberStatus[sel-1]%N_SIZE;
+	
+	bingoBoard[row][col] = BINGONUM_HOLE;
+	numberStatus[sel-1] = BINGONUM_HOLE;
+
 }
 
 int bingo_countCompletedLine(void)
 {
+	int i,j;
+	int cnt = 0;
+	int checkBingo;
 	
+	//row
+	for (i=0;i<N_SIZE;i++){ //i가 3이면 3번째 줄을 고려하고 있는것이다. 
+		checkBingo = 1;
+		for(j=0;j<N_SIZE;j++){
+			if(bingoBoard[i][j] > 0){
+				checkBingo = 0;
+				break;
+				
+			}
+		}
+		if (checkBingo == 1){
+			 cnt++;
+		}
+	}
+	//col
+	for (j=0;j<N_SIZE;j++){ //i가 3이면 3번째 줄을 고려하고 있는것이다. 
+		checkBingo = 1;
+		for(i=0;i<N_SIZE;i++){
+			if(bingoBoard[i][j] > 0){
+				checkBingo = 0;
+				break;
+				
+			}
+		}
+		if (checkBingo == 1){
+			 cnt++;
+		}
+	}
+	
+	//diagonal
+	checkBingo = 1;
+	for (i=0;i<N_SIZE;i++){
+		if(bingoBoard[i][j] > 0){
+			checkBingo = 0;
+			break;
+		}
+	}
+	if(checkBingo ==1)
+	     cnt++;
+	     
+    checkBingo = 1;
+	for (i=0;i<N_SIZE;i++){
+		if(bingoBoard[i][N_SIZE-i-1] > 0){
+			checkBingo = 0;
+			break;
+		}
+	}
+	if(checkBingo ==1)
+	     cnt++;
+	     
+     return cnt;
 }
--- main.c
+++ main.c
@@ -3,7 +3,38 @@
 #include <time.h>
 #include "bingoBoard.h"
 
+#define BINGO_RES_UNFINSHED   -1
+#define BINGO_RES_FINSHED      0
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int check_gameEnd(void)
+{
+	int res = BINGO_RES_UNFINSHED;
+	if(bingo_countCompletedLine() >= N_LINE){
+		res = BINGO_RES_FINSHED;
+	}
+	
+	return res;
+}
+
+int get_number(void)
+{
+	int selNum = 0;
+	do{
+		printf("select a number : ");
+		scanf("%d", &selNum);
+		fflush(stdin);
+		
+		if(selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT)
+		{
+			printf("%i is not on the board! select other one.\n", selNum);
+		}
+		
+	} while(selNum < 1 || selNum > N_SIZE*N_SIZE || bingo_checkNum(selNum) == BINGO_NUMSTATUS_ABSENT);
+	
+	return selNum;
+}
 
 int main(int argc, char *argv[]) {
 	
@@ -18,12 +49,18 @@ int main(int argc, char *argv[]) {
 	
 	//game
 	bingo_init();
-	bingo_print();
-	bingo_inputNum(21);
-	bingo_print();
 	
 	//initialize bingo board 일단 빙고보드부터 만들기 
-	
+	while(check_gameEnd() == BINGO_RES_UNFINSHED)
+	{
+		bingo_print();
+		
+		printf("No. of completed line : %i\n", bingo_countCompletedLine());
+		
+		int selNum = get_number();
+		
+		bingo_inputNum(selNum);
+	}
 	
 	/*
 	while (game is not end) 반복하려고 while문 사용/ 게임이 끝나면 반복문을 빠져나감.// 줄 수가 N_BINGO보다 작음 
